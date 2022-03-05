9c7c1e2dd9410f2d7d98e7efe5f92fa9a8fb8e58
pifh <pifh@free.fr>
2022-03-05 11:58:55 +0100

    CarteUtil

--- models.py
+++ models.py
@@ -5,6 +5,11 @@ class Carte:
       self.verso = verso
       self.isReversible = isReversible
 
+class CarteUtil(Carte):
+  def __init__(self, id, recto, verso, isReversible, date=0):
+    Carte.__init__(self, id, recto, verso, isReversible)
+    self.date = date
+
 class Paquet:
   def __init__(self, id, listeCartes = []):
     self.id = id
@@ -57,16 +62,16 @@ def printSession(session: Session) :
       print("    "+carte.recto)
 
 paquet = Paquet(1, [
-  Carte(1, "Question 1", "Réponse 1", True),
-  Carte(2, "Question 2", "Réponse 2", True),
-  Carte(3, "Question 3", "Réponse 3", True),
-  Carte(4, "Question 4", "Réponse 4", True),
-  Carte(5, "Question 5", "Réponse 5", True),
-  Carte(6, "Question 6", "Réponse 6", True),
-  Carte(7, "Question 7", "Réponse 7", True),
-  Carte(8, "Question 8", "Réponse 8", True),
-  Carte(9, "Question 9", "Réponse 9", True),
-  Carte(10, "Question 10", "Réponse 10", True)
+  CarteUtil(1, "Question 1", "Réponse 1", True),
+  CarteUtil(2, "Question 2", "Réponse 2", True),
+  CarteUtil(3, "Question 3", "Réponse 3", True),
+  CarteUtil(4, "Question 4", "Réponse 4", True),
+  CarteUtil(5, "Question 5", "Réponse 5", True),
+  CarteUtil(6, "Question 6", "Réponse 6", True),
+  CarteUtil(7, "Question 7", "Réponse 7", True),
+  CarteUtil(8, "Question 8", "Réponse 8", True),
+  CarteUtil(9, "Question 9", "Réponse 9", True),
+  CarteUtil(10, "Question 10", "Réponse 10", True)
 ])
 
 carte1 = paquet.listeCartes[0]
@@ -80,6 +85,7 @@ for n in range(0, len(masession.compartiments)):
   print("stop"+str(n))
 masession.baisser(carte1)
 printSession(masession)
+print(carte1.date)
 
 
 
